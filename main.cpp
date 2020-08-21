29b16dd680c4d93665be8e0388b43ca91e5adcfd
MajesticWaffle <tskup7@gmail.com>
2020-08-20 22:25:55 -0500

    stuff

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 using namespace std;
 
 
-
 /**Hey there, there's literally no comments here, and the program itself is stupidly inefficient**/
 
 void OutputChar(int charIndex);
@@ -27,64 +26,77 @@ int ExecuteBFCode(char memblock[], int count, int location, int micro, int i);
 bool my_predicate(char c);
 
 int main(int argc, char *argv[]) { //File location, Delay (MS), Cell Count, Starting Cell
-int cellCount = 8;
-int pointerLocation = 0;
-int delayMicro = 50;
-std::string filePath;
+    int cellCount = 8;
+    int pointerLocation = 0;
+    int delayMicro = 50;
+    std::string filePath;
 
-if(argc == 1){
-    DisplayHelp();
-    return 0;
+    if (argc == 1) {
+        DisplayHelp();
+        return 0;
+    }
+
+    bool loadingfile = true;
+    string code;
+
+    if (argc >= 2) {
+        if (argv[1] == std::string("/I") || argv[1] == std::string("/i")) {
+            loadingfile = false;
+            cout << "Input brainfuck code:" << "\n";
+            cin >> code;
+        } else {
+            filePath = argv[1];
+        }
+    }
+    if (argc >= 3) {
+        delayMicro = atoi(argv[2]);
+    }
+    if (argc >= 4) {
+        cellCount = atoi(argv[3]);
+    }
+    if (argc >= 5) {
+        pointerLocation = atoi(argv[4]);
+    }
+    if (loadingfile) {
+        string s;
+        string line;
+        ifstream file(filePath);
+
+        if (!file.is_open()) {
+            cout << "Failed to open file: " << filePath;
+            return -1;
+        }
+
+        while (getline(file, line)) {
+            s += line;
+        }
+        file.close();
+
+        s.erase(std::remove_if(s.begin(), s.end(), my_predicate), s.end());
+        code = s;
+    }
+
+    int n = code.length();
+    char char_array[n + 1];
+    strcpy(char_array, code.c_str());
+
+    ExecuteBFCode(char_array, cellCount, pointerLocation, delayMicro, n);
+
+    system("pause");
+    system("cls");
 }
 
-if(argc >= 2){
-    filePath = argv[1];
-}
-if(argc >= 3){
-    delayMicro = atoi(argv[2]);
-}
-if(argc >= 4){
-    cellCount = atoi(argv[3]);
-}
-if(argc >= 5){
-    pointerLocation = atoi(argv[4]);
-}
-
-string s;
-string line;
-ifstream file (filePath);
-
-if(!file.is_open()){
-    cout << "Failed to open file: " << filePath;
-    return -1;
-}
-
-while(getline(file, line)){
-    s = s + line;
-}
-file.close();
-
-s.erase(std::remove_if(s.begin(), s.end(), my_predicate), s.end());
-
-int n = s.length();
-char char_array[n+1];
-strcpy(char_array, s.c_str());
-
-ExecuteBFCode(char_array, cellCount, pointerLocation, delayMicro, n);
-
-system("pause");
-system("cls");
-}
-
-bool my_predicate(char c){
+bool my_predicate(char c) {
     return !(c == '>' || c == '<' || c == '+' || c == '-' || c == '[' || c == ']' || c == '.' || c == ',');
 }
 
 void DisplayHelp() {
-    std::cout << "bfv.exe {File Path} [Delay] [Cell Count] [Starting Cell]" << "\n" << "{} = Required" << "\n" << "[] = Optional";
+    std::cout << "bfv.exe {File Path} [Delay] [Cell Count] [Starting Cell]";
+    std::cout << "\n or: \n";
+    std::cout << "bfv.exe /I [Delay] [Cell Count] [Starting Cell]";
 }
 
-int ExecuteBFCode(char code[], int cellCount, int pointerLocation, int Delay, int size){
+int ExecuteBFCode(char code[], int cellCount, int pointerLocation, int Delay, int size) {
     int cells[cellCount];
     int interpreterLocation = 0; //The current char being interpreted by the compiler
 
@@ -110,14 +122,14 @@ int ExecuteBFCode(char code[], int cellCount, int pointerLocation, int Delay, in
                 break;
             case '+':
                 cells[pointerLocation]++;
-                if(cells[pointerLocation] > 255){
+                if (cells[pointerLocation] > 255) {
                     cells[pointerLocation] = 0;
                 }
                 interpreterLocation++;
                 break;
             case '-':
                 cells[pointerLocation]--;
-                if(cells[pointerLocation] < 0){
+                if (cells[pointerLocation] < 0) {
                     cells[pointerLocation] = 255;
                 }
                 interpreterLocation++;
@@ -159,7 +171,6 @@ int ExecuteBFCode(char code[], int cellCount, int pointerLocation, int Delay, in
 }
 
 
-
 int LoopExitLocation(int startingIndex, char array[], int size) {
     bool ignoreNext = false; //Set if enters a new loop
     for (int i = startingIndex + 1; i <= size; i++) {
@@ -196,7 +207,7 @@ int LoopStartLocation(int startingIndex, char array[]) {
 }
 
 void OutputChar(int charIndex) {
-    output::DisplayChar((char)charIndex);
+    output::DisplayChar((char) charIndex);
 }
 
 int inputChar() {
